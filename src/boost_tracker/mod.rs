642505a6e477a99609f9e2fd5e55990917da2e81
kadu-v <kazuonsunday@yahoo.co.jp>
2026-01-18 18:34:59 +0900

    Add: assoc module for boost_tracker

--- src/boost_tracker/mod.rs
+++ src/boost_tracker/mod.rs
@@ -1,4 +1,6 @@
+pub mod assoc;
 mod kalman_filter;
 
 pub use crate::rect::Rect;
+pub use assoc::{associate, iou_batch, linear_assignment, AssignmentResult, AssociateParams};
 pub(crate) use kalman_filter::KalmanFilter;
